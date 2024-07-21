975786e47a6c9a49064af6b50fb872ede2a43612
Koniesjay <jkonyeri@gmail.com>
2024-07-21 08:26:55 +0100

    first commit

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,40 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  title: "matchcast"
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div style={{ display: "flex" }} tw="">
+        <img src='https://i.ibb.co/pfzzR80/match.png' alt='' tw="" />
+      </div>
+    ),
+    imageAspectRatio: "1:1",
+    imageOptions: { width: 600, height: 600 },
+    intents: [
+      <Button.Link href="https://match-cast.vercel.app/">Open App</Button.Link>
+    ],
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
